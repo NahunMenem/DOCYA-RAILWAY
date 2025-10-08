600c5f88208ba3386b22eccdfd5c9e9c50b319a6
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-10-08 12:05:36 -0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,7 @@
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    print(f"🚀 Iniciando servidor en puerto {port}")
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
