413a91362f6de520038a622cf08dd130b33f8120
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-10-08 11:41:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "Entrando a start.sh..."
+echo "🚀 Iniciando contenedor DOCYA..."
+
 if [ -z "$PORT" ]; then
   echo "⚠️  Variable PORT no detectada, usando 8080 por defecto"
   PORT=8080
 else
   echo "✅ Puerto detectado: $PORT"
 fi
+
+echo "🌐 Levantando servidor en puerto $PORT..."
 exec uvicorn main:app --host 0.0.0.0 --port $PORT
