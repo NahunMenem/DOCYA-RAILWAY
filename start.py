ed7fab92e2953604a4f5c0c73fc80ba743b2d357
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-10-08 18:41:15 -0300

    Update start.py

--- start.py
+++ start.py
@@ -4,4 +4,10 @@ import uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     print(f"🚀 Iniciando servidor DOCYA en puerto {port}")
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        workers=1  # 👈 fuerza un solo proceso para compartir memoria global
+    )
