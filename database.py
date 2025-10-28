1fe44d19a3d943333cff8ae0a7f2751654668b6c
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-10-28 11:32:44 -0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_db():
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+    try:
+        yield conn
+    finally:
+        conn.close()
